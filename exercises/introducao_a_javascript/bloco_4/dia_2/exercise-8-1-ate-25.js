c86ad93f944214f50f25dc339628642034c47c36
Arlen Freitas <arlenfreitas1@gmail.com>
2021-02-26 09:57:23 -0300

    1 ate 25

---
+++ exercises/introducao_a_javascript/bloco_4/dia_2/exercise-8-1-ate-25.js
@@ -0,0 +1,9 @@
+//Exercise 8: Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = [];
+
+for (let i = 1; i <= 25; i+=1) {
+  resultado.push(i);
+}
+
+console.log(resultado);
