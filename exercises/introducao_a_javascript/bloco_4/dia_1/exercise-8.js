80c2790eefb204b5fd459d05441bc37790273041
Arlen Freitas <arlenfreitas1@gmail.com>
2021-02-25 21:36:01 -0300

    corrige exerc-8

--- exercises/introducao_a_javascript/bloco_4/dia_1/exercise-8.js
+++ exercises/introducao_a_javascript/bloco_4/dia_1/exercise-8.js
@@ -1,9 +1,9 @@
 //exercise 08
 let numA = 13;
-let numB = 20;
+let numB = 10;
 let numC = 25;
 
-if ((numA, numB, numC) % 2) {
+if (numA % 2 == 0 || numB % 2 == 0 || numC % 2 == 0) {
   console.log("True")
 }
 else {
