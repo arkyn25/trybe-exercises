46605645305f90924ebb7f01b87b34e5476db92d
Arlen Freitas <arlenfreitas1@gmail.com>
2021-04-30 18:08:38 -0300

    pokedex2

--- exercises/componentes-com-estado-eventos-e-formularios-com-react.1/bloco_12/dia_1/my-pokedex/src/Pokedex.js
+++ exercises/componentes-com-estado-eventos-e-formularios-com-react.1/bloco_12/dia_1/my-pokedex/src/Pokedex.js
@@ -28,15 +28,25 @@ class Pokedex extends React.Component {
       )
     }
   }
+  setType(type) {
+    this.setState(() => ({ index: 0 }))
+    const { pokemons } = this.props;
+    if (type !== 'All') {
+      this.setState(() => ({ pokeList: pokemons.filter(pokemon => pokemon.type === type) }))
+    } else this.setState(() => ({ pokeList: pokemons.map(pokemon => pokemon) }))
+  }
 
   render() {
     const { pokeList, index } = this.state;
-    
+    const typeList = ['All', 'Fire', 'Psychic', 'Electric', 'Normal', 'Bug', 'Poison', 'Dragon'];
     return (
       <div>
         <div className="pokedex">
           {<Pokemon key={pokeList[index].id} pokemon={pokeList[index]} />}
           {this.buttons()}
+        </div>
+        <div>
+          {typeList.map(type => <button key={type} className={type} onClick={this.setType.bind(this, type)}>{type}</button>)}
         </div>
       </div>
     );
