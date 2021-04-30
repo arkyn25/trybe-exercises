32fb57290e7b2597d597e8c0515fdd8fd1c41021
Arlen Freitas <arlenfreitas1@gmail.com>
2021-04-30 00:49:01 -0300

    pokedex

--- exercises/componentes-com-estado-eventos-e-formularios-com-react.1/bloco_12/dia_1/my-pokedex/src/Pokedex.js
+++ exercises/componentes-com-estado-eventos-e-formularios-com-react.1/bloco_12/dia_1/my-pokedex/src/Pokedex.js
@@ -2,11 +2,43 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setUp = this.setUp.bind(this);
+    this.state = {
+      index: 0,
+      pokeList: this.props.pokemons.map(pokemon => pokemon),
+  
+    };
+  }
+  setUp() {
+    const { index, pokeList } = this.state;
+    if (index === pokeList.length - 1) {
+      this.setState(() => ({ index: 0 }))
+    } else this.setState((prev) => ({ index: prev.index + 1}) )
+  }
+  buttons() {
+    const { pokeList } = this.state;
+
+    if (pokeList.length > 1) {
+      return (
+        <div>
+          <button onClick={this.setUp}>Proximo Pokemon</button>
+        </div>
+      )
+    }
+  }
+
   render() {
+    const { pokeList, index } = this.state;
+    
     return (
-      <section className="pokedex">
-        {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-      </section>
+      <div>
+        <div className="pokedex">
+          {<Pokemon key={pokeList[index].id} pokemon={pokeList[index]} />}
+          {this.buttons()}
+        </div>
+      </div>
     );
   }
 }
